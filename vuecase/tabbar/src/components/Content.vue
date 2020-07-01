14b12b8e556fa82bc6300b7fa4f2999f2c8b8dde
lixin <lx282555947@163.com>
2020-07-01 18:06:14 +0800

    vue learn

---
+++ vuecase/tabbar/src/components/Content.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>这是内容</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Content"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuecase/tabbar/src/components/Item.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div><img src="../assets/img/" alt=""></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Item"
+    }
+</script>
+
+<style scoped>
+
+</style>
